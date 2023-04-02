5240e860e173c18d637c6bcacf472b7b400f06f1
JJHaEun <yj05037@gmail.com>
2023-04-02 12:03:48 +0900

    이전커밋으로 되돌아가기

--- groomeong_front/src/components/commons/libraries/GetLatLngBounds.ts
+++
@@ -1,9 +0,0 @@
-import { IDataProps } from "./../GeoData/getGeoData";
-
-export const getLatLngBounds = (map: IDataProps) => {
-  const bounds = new google.maps.LatLngBounds();
-  map.geometry.forEach((coord) => {
-    bounds.extend(coord);
-  });
-  return bounds;
-};
